40a010e8685e8b6f08c55f52b4e2c763a4025234
Sergio Soneira <sergio.soneira@gmail.com>
2024-11-08 12:05:41 +0100

    S0202

---
+++ S0202/Assets/Move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    float speed = 0.5f;
+
+    void Start() {
+        direction = goal.transform.position - this.transform.position;
+        }
+
+    private void LateUpdate() {
+        Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+        this.transform.position += velocity;
+    }
+}
